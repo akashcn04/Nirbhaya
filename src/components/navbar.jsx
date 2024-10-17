1839760fb6835f03e9ce9824bf24479b962dbf8c
akashcn04 <akashnyamagoud45@gmail.com>
2024-10-17 18:28:12 +0530

    updated navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon2 from "../../public/assets/icon2.png";
-import About from "./about";
 import { Link } from "react-scroll";
-import Issues from "./womenIssues";
-import Model from "./Model";
-import Contact from "./contact";
+import { FaBars, FaTimes } from "react-icons/fa"; // For hamburger and close icons
 
 function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
   return (
     <div
       className="w-screen h-[80px] bg-slate-100 py-4 shadow-[0px_0px_20px_5px_white]"
@@ -15,24 +18,23 @@ function Navbar() {
       <div className="flex justify-between items-center max-w-[1320px] mx-auto px-4 md:px-8 lg:px-12">
         {/* Logo section */}
         <div className="flex items-center gap-4">
-          <img
-            src={Icon2}
-            alt="NIRBHAYA"
-            className="w-[40px] md:w-[50px]"
-          />
+          <img src={Icon2} alt="NIRBHAYA" className="w-[40px] md:w-[50px]" />
           <div className="font-semibold text-xl md:text-2xl lg:text-3xl">
             NIRBHAYA
           </div>
         </div>
 
-        {/* Navbar links */}
+        {/* Desktop Navbar links */}
         <div className="hidden md:flex gap-6 lg:gap-9 text-sm md:text-base lg:text-[18px]">
           <li className="list-none cursor-pointer duration-[0.5s] hover:scale-110">
-            <Link to="/" onClick={() => window.location.reload()} className="font-semibold transition duration-[0.5s] transform hover:scale-105">
+            <Link
+              to="/"
+              onClick={() => window.location.reload()}
+              className="font-semibold transition duration-[0.5s] transform hover:scale-105"
+            >
               Home
             </Link>
           </li>
-
           <li className="list-none font-semibold transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
             <Link
               to="about"
@@ -44,7 +46,6 @@ function Navbar() {
               About
             </Link>
           </li>
-
           <li className="list-none font-semibold transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
             <Link
               to="issues"
@@ -56,7 +57,6 @@ function Navbar() {
               Issues
             </Link>
           </li>
-
           <li className="list-none font-semibold transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
             <Link
               to="model"
@@ -68,7 +68,6 @@ function Navbar() {
               Model
             </Link>
           </li>
-
           <li className="list-none font-semibold transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
             <Link
               to="contact"
@@ -81,63 +80,91 @@ function Navbar() {
             </Link>
           </li>
         </div>
+
+        {/* Hamburger Icon for Mobile */}
+        <div className="md:hidden">
+          <button onClick={toggleNav} className="text-2xl">
+            {navOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
 
       {/* Mobile Navigation */}
-      <div className="flex justify-center md:hidden mt-2">
-        <div className="flex flex-col items-center gap-4">
-          <ul className="flex flex-col items-center gap-2 text-sm font-semibold">
-            <li className="cursor-pointer duration-[0.5s] hover:scale-110">
-              <Link to="/" onClick={() => window.location.reload()} className="transition duration-[0.5s] transform hover:scale-105">
-                Home
-              </Link>
-            </li>
-            <li className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
-              <Link
-                to="about"
-                smooth={true}
-                duration={500}
-                offset={-70}
-                activeClass="active"
-              >
-                About
-              </Link>
-            </li>
-            <li className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
-              <Link
-                to="issues"
-                smooth={true}
-                duration={500}
-                offset={-70}
-                activeClass="active"
-              >
-                Issues
-              </Link>
-            </li>
-            <li className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
-              <Link
-                to="model"
-                smooth={true}
-                duration={500}
-                offset={-70}
-                activeClass="active"
-              >
-                Model
-              </Link>
-            </li>
-            <li className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer">
-              <Link
-                to="contact"
-                smooth={true}
-                duration={500}
-                offset={-70}
-                activeClass="active"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <div
+        className={`${
+          navOpen ? "block" : "hidden"
+        } md:hidden absolute w-full left-0 top-[80px] bg-slate-100 shadow-lg z-10`}
+      >
+        <ul className="flex flex-col items-center gap-4 py-4 text-sm font-semibold">
+          <li className="cursor-pointer duration-[0.5s] hover:scale-110">
+            <Link
+              to="/"
+              onClick={() => {
+                window.location.reload();
+                toggleNav();
+              }}
+              className="transition duration-[0.5s] transform hover:scale-105"
+            >
+              Home
+            </Link>
+          </li>
+          <li
+            className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer"
+            onClick={toggleNav}
+          >
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              activeClass="active"
+            >
+              About
+            </Link>
+          </li>
+          <li
+            className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer"
+            onClick={toggleNav}
+          >
+            <Link
+              to="issues"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              activeClass="active"
+            >
+              Issues
+            </Link>
+          </li>
+          <li
+            className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer"
+            onClick={toggleNav}
+          >
+            <Link
+              to="model"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              activeClass="active"
+            >
+              Model
+            </Link>
+          </li>
+          <li
+            className="transition duration-[0.5s] transform hover:scale-110 cursor-pointer"
+            onClick={toggleNav}
+          >
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              activeClass="active"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
